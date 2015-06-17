4c0462f0528204f4c907d910ff1f68e45abe4a55
Travis Herzog <travisherzog@hds-log.com>
2015-06-16 23:34:21 -0500

    Removed telephone number requirement from customer API

--- catalog/controller/api/customer.php
+++ catalog/controller/api/customer.php
@@ -51,9 +51,11 @@ class ControllerApiCustomer extends Controller {
 				$json['error']['email'] = $this->language->get('error_email');
 			}
 
+			/* Remove telephone number requirement
 			if ((utf8_strlen($this->request->post['telephone']) < 3) || (utf8_strlen($this->request->post['telephone']) > 32)) {
 				$json['error']['telephone'] = $this->language->get('error_telephone');
 			}
+			*/
 
 			// Customer Group
 			if (isset($this->request->post['customer_group_id']) && is_array($this->config->get('config_customer_group_display')) && in_array($this->request->post['customer_group_id'], $this->config->get('config_customer_group_display'))) {
